68b0de8c7254e633d5fa657872c0606085263961
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-07-13 21:49:12 +0900

    Create cuttingchoco_2163.java

---
+++ cuttingchoco_2163.java
@@ -0,0 +1,22 @@
+//2163¹®Á¦
+
+import java.util.Scanner;
+
+
+public class cuttingchoco_2163 {
+
+	public static void main(String args[]) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n, m;
+		
+		n=scan.nextInt();
+		m=scan.nextInt();
+		
+		int ans = n*m-1;
+		
+		System.out.println(ans);
+	}
+	
+}
