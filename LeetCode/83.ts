fa795e0802f7cedffe3befea34685b529edce98e
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2023-06-20 23:55:07 +0900

    Leet 83. Remove Duplicates from Sorted List

---
+++ LeetCode/83.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let node = head;
+    while(node?.next) {
+        while(node.val === node.next?.val) {
+            node.next = node.next?.next;
+        }
+        node = node.next;
+    }
+
+    return head;
+};
